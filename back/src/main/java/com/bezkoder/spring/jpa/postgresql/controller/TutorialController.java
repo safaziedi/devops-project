1b1acf62e3b48170410417667a9355a1f26336c8
safaziedi <safaziedi070@gmail.com>
2023-12-01 17:51:28 +0100

    update ports

--- back/src/main/java/com/bezkoder/spring/jpa/postgresql/controller/TutorialController.java
+++ back/src/main/java/com/bezkoder/spring/jpa/postgresql/controller/TutorialController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bezkoder.spring.jpa.postgresql.model.Tutorial;
 import com.bezkoder.spring.jpa.postgresql.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
