e892ae3aaaf6cd0fb6c8e20aaa42333b6a78cad5
safaziedi <safaziedi070@gmail.com>
2023-12-24 13:28:58 +0100

    docker compose

--- back/src/main/java/com/bezkoder/spring/jpa/postgresql/controller/TutorialController.java
+++ back/src/main/java/com/bezkoder/spring/jpa/postgresql/controller/TutorialController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bezkoder.spring.jpa.postgresql.model.Tutorial;
 import com.bezkoder.spring.jpa.postgresql.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
